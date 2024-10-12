f8e4947b652d0ec558d934d667ac77eaa8b7b9ea
SBA-Yone <s.yonezawa@make-tomorrow.com>
2024-10-12 19:18:07 +0900

    ActiveHashを導入する

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
 
   has_many :items
   has_one_attached :image
 
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id, npresence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sales_status_id, presence: true
   validates :shipping_fee_id, presence: true
   validates :prefecture_id, presence: true
