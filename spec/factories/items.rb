fe41868807b8460a5c455ba9121f219739db5ecf
SBA-Yone <s.yonezawa@make-tomorrow.com>
2024-10-15 18:09:25 +0900

    モデルの単体テストコードを書く（商品出品機能）

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { Faker::Commerce.product_name }
+    info                  { Faker::Lorem.paragraph }
+    price                 { Faker::Number.between(from: 300, to: 9_999_999) }
+    category_id           { Faker::Number.between(from: 1, to: 10) }
+    sales_status_id       { Faker::Number.between(from: 1, to: 10) }
+    shipping_fee_id       { Faker::Number.between(from: 1, to: 10) }
+    prefecture_id         { Faker::Number.between(from: 1, to: 47) }
+    scheduled_delivery_id { Faker::Number.between(from: 1, to: 10) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
