484599256c2e7f613f4bad26048371bc6e40c4f9
SBA-Yone <s.yonezawa@make-tomorrow.com>
2024-10-23 11:03:02 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update ,:destroy]
   before_action :check_item_owner, only: [:edit, :update]
 
   def new
@@ -33,6 +33,12 @@ class ItemsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+   @item.destroy
+   redirect_to items_path
+  end
+
 
   private
 
