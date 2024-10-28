3c781f779191dd113fc022942995b1d3822795f7
SBA-Yone <s.yonezawa@make-tomorrow.com>
2024-10-28 19:27:50 +0900

    コードレビュー後修正2(商品購入機能)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :purchase
+  has_many :purchases
 
   validates :nickname, presence: true
   validates :password,
