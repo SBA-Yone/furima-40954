22645fb9f227b50ee0df39ccaff1db64bf9d95ae
SBA-Yone <s.yonezawa@make-tomorrow.com>
2024-10-29 11:26:59 +0900

    フリマアプリ挙動後修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,8 @@ class User < ApplicationRecord
   has_many :purchases
 
   validates :nickname, presence: true
-  validates :password,
-            format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!-~]).+\z/,
-                      message: 'is invalid. Include both letters and numbers' }
-  validate :password_no_full_width
+  validates :password, presence: true
+  validate :password_complexity
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
   validates :first_name, presence: true,
                          format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
@@ -19,9 +17,15 @@ class User < ApplicationRecord
                               format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :birth_date, presence: true
 
-  def password_no_full_width
-    return unless password =~ /[^\x20-\x7E]/
+  private
 
-    errors.add(:password, 'is invalid. Include both letters and numbers')
+  def password_complexity
+    if password.present?
+      if password =~ /[^\x20-\x7E]/
+        errors.add(:password, 'is invalid. Include both letters and numbers')
+      elsif password !~ /\A(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!-~]).+\z/
+        errors.add(:password, 'is invalid. Include both letters and numbers')
+      end
+    end
   end
 end
